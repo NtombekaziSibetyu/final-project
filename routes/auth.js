3b584498f783ed1234d090eec2c2ac4121eabb55
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-07-15 17:51:18 +0200

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check')
+const Patients = require('../models/Patients');
+
 
 //route GET api/auth
 // login a patient
@@ -15,6 +21,50 @@ router.put('/:id', (req, res) => { res.send(req.body) });
 //route POST api/auth
 // auth patient and get token
 // public access
-router.post('/', (req, res) => { res.send(req.body) });
+router.post('/',
+[
+    check('name','Please enter aa valid full name').not().isEmpty(),
+    check('identityNo','Please enter a valid identity number').exists()
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()})
+        }
+
+    const {name, identityNo } = req.body;
+
+    try {
+        let patient = await Patients.findOne({ name });
+
+        if(!patient){
+            return res.status(400).json({msg:'invalid credentials'});
+        }
+
+        const isMatch = bcrypt.compare(identityNo, patient.identityNo);
+
+        if(!isMatch){
+            return res.status(400).json({msg:'invalid credentials'})
+        }
+        const payload = {
+            patient: {
+                id: patient.id
+            }
+        }
+
+        jwt.sign( payload, config.get('jwtSecret'),{
+            expiresIn:360000
+        }, 
+        (err, token) => {
+            if(err) throw err;
+            res.json({token})
+        })
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
+});
 
 module.exports = router;
--- routes/patients.js
+++ routes/patients.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check')
 const Patients = require('../models/Patients');
 
@@ -11,12 +14,54 @@ router.post('/', [
     check('identityNo', 'ID number is required').isLength({min:13}),
     check('email', 'please include a valid email').isEmail(),
     check('phone','please a valid cell number').isLength({ min:10})
-], (req, res) => { 
+], 
+async(req, res) => { 
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array()})
         }
-        res.send('passed')
+        
+        const { name, identityNo, email, phone, address} = req.body;
+
+        try {
+            let patient = await Patients.findOne({ identityNo })
+
+            if(patient) {
+                return res.status(400).json({ msg: 'The ID number you have entered is already registered/ belongs to someone else'})
+            }
+
+            patient = new Patients({
+                name,
+                identityNo,
+                email,
+                phone,
+                address
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            patient.identityNo = await bcrypt.hash(identityNo, salt);
+
+            await patient.save();
+
+            const payload = {
+                patient: {
+                    id: patient.id
+                }
+            }
+
+            jwt.sign( payload, config.get('jwtSecret'),{
+                expiresIn:360000
+            }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({token})
+            })
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('server error');
+        }
     }
 )
 module.exports = router;
