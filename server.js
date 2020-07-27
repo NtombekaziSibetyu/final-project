f6ae842d77d8dcce3593dc7bb2b88989114acca6
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-07-27 18:29:33 +0200

    commit

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
 
-
 //connect the database
 connectDB();
 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve( '/client','/build', '/index.html'))
+    });
+}
+
+
 //initialize middleware
 app.use(express.static('client/build'));
+app.get('/', (req, res) => res.sendFile(path.resolve( '/client','/build', '/index.html')));
 
-app.get('/', (req, res) => res.json({ msg: 'clinic booking api'}));
 //routes 
 app.use('/api/patients', require('./routes/patients'));
 app.use('/api/auth', require('./routes/auth'));
@@ -22,11 +31,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
 console.log(`server started on port ${PORT}`));
 
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(req, 'client','build', 'index.html'))
-    });
-}
