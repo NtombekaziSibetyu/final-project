310c31c682687eb87a8792b24e10f65e98dda38a
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-07-28 10:59:17 +0200

    changes

---
+++ client/build/precache-manifest.4d882b23773d5a25a026f0b26a0c6f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa57cbdf9345d3509d63b716f8abb34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae6486dd23e53c98db3",
+    "url": "/static/css/main.92cea844.chunk.css"
+  },
+  {
+    "revision": "ea64826b1dbebc7b1e30",
+    "url": "/static/js/2.88cb98c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88cb98c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae6486dd23e53c98db3",
+    "url": "/static/js/main.ba0829bd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
