2a1116d0666514c240e97a96787b07491efdff0b
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-08-03 15:42:16 +0200

    Refactor.

--- server.js
+++ server.js
@@ -14,16 +14,20 @@ app.use('/api/patients', require('./routes/patients'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/bookings', require('./routes/bookings'));
 
+
 //serve static production
 if( process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => 
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html')));
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html')))
 } else {
     //remove this 
-    app.get('/',(req, res) => res.json({ msg: 'clinic API'}));
+app.get('/',(req, res) => res.json({ msg: 'clinic API'}))
 
 }
+
+//use port 
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
 console.log(`server started on port ${PORT}`));
